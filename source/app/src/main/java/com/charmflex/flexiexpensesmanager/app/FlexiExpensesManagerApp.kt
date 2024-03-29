97eb998c35d7d861026a29814f1496e789f815ab
ext.jiaming.luk <ext.jiaming.luk@grabtaxi.com>
2024-03-29 22:17:27 +0800

    add network builder

--- source/app/src/main/java/com/charmflex/flexiexpensesmanager/app/FlexiExpensesManagerApp.kt
+++ source/app/src/main/java/com/charmflex/flexiexpensesmanager/app/FlexiExpensesManagerApp.kt
@@ -1,7 +1,9 @@
 package com.charmflex.flexiexpensesmanager.app
 
 import android.app.Application
+import androidx.collection.mutableIntListOf
 
 class FlexiExpensesManagerApp : Application() {
     // jira - github integration test
+
 }
