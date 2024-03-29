1e77de8f2bdc0025637de71937418e9af7467214
ext.jiaming.luk <ext.jiaming.luk@grabtaxi.com>
2024-03-29 23:23:57 +0800

    added app theme, added sample screen with buttons

--- source/app/src/main/java/com/charmflex/flexiexpensesmanager/MainActivity.kt
+++ source/app/src/main/java/com/charmflex/flexiexpensesmanager/MainActivity.kt
@@ -3,14 +3,24 @@ package com.charmflex.flexiexpensesmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.charmflex.flexiexpensesmanager.ui.theme.FlexiExpensesManagerTheme
+import com.charmflex.flexiexpensesmanager.ui_common.SGLargeGhostButton
+import com.charmflex.flexiexpensesmanager.ui_common.SGLargePrimaryButton
+import com.charmflex.flexiexpensesmanager.ui_common.SGLargeSecondaryButton
+import com.charmflex.flexiexpensesmanager.ui_common.SGScaffold
+import com.charmflex.flexiexpensesmanager.ui_common.grid_x2
+import com.charmflex.flexiexpensesmanager.ui_common.grid_x3
+import com.example.compose.FlexiExpensesManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +28,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             FlexiExpensesManagerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                SGScaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Greeting("Android")
+                    Spacer(modifier = Modifier.weight(1f))
+                    SGLargePrimaryButton(modifier = Modifier.fillMaxWidth(), text = "Primary Button") {
+                        
+                    }
+                    Spacer(modifier = Modifier.height(grid_x2))
+                    SGLargeSecondaryButton(modifier = Modifier.fillMaxWidth(), text = "Secondary Button") {
+                        
+                    }
+                    Spacer(modifier = Modifier.height(grid_x2))
+                    SGLargeGhostButton(modifier = Modifier.fillMaxWidth(), text = "ghost") {
+                        
+                    }
                 }
             }
         }
